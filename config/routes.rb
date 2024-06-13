04ef68e9c8f77c25c6fdf6f3888c475e9cb963a1
shehan-jayasinghe <jayasingheshehan1995@gmail.com>
2024-06-13 07:58:11 +0530

    added link helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  #GET /about
-  get "about", to:"about#index"
+  get "about-us",to:"about#index",as:"about"
   "get /   get '/', to:'about#main#index'"
   root to:"main#index"
 end
