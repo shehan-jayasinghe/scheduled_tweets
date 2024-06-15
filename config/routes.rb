4dd07cc89291b313c81eae0ff88d099b7d374109
shehan-jayasinghe <jayasingheshehan1995@gmail.com>
2024-06-15 11:36:42 +0530

    Current user modele

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get "about-us",to:"about#index",as:"about"
-  get "sign_up" , to:"registration#new"
-  post "sign_up" , to:"registration#create"
+  get "sign_up" , to:"registrations#new"
+  post "sign_up" , to:"registrations#create"
+  get "sign_in" , to:"sessions#new"
+  post "sign_in" , to:"sessions#create"
   delete "logout", to: "sessions#destroy"
   get 'sw.js', to: 'service_worker#service_worker'
   "get /   get '/', to:'about#main#index'"
