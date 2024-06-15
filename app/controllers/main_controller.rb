c46f21af04ac5e18f3b0042cddf276d5e7158655
shehan-jayasinghe <jayasingheshehan1995@gmail.com>
2024-06-15 09:11:48 +0530

    login with session cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,5 +3,8 @@ class MainController < ApplicationController
   def index
      flash[:notice] = "Logged in Successfully"
      flash[:alert] = "Invalid email or password"
+     if session[:user_id]
+       @user = User.find(session[:user_id])
+     end  
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "about-us",to:"about#index",as:"about"
+  get "sign_up" , to:"registration#new"
+  post "sign_up" , to:"registration#create"
   "get /   get '/', to:'about#main#index'"
   root to:"main#index"
 end
