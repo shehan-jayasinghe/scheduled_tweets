08f14444ab25fef39c03444754f00cdc845cf30c
shehan-jayasinghe <jayasingheshehan1995@gmail.com>
2024-06-15 14:36:26 +0530

    edit password

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   get "about-us",to:"about#index",as:"about"
+
   get "sign_up" , to:"registrations#new"
   post "sign_up" , to:"registrations#create"
+
+  get "password", to: "passwords#edit", as:"edit_password"
+  patch "password", to: "passwords#update"
+
   get "sign_in" , to:"sessions#new"
   post "sign_in" , to:"sessions#create"
+
   delete "logout", to: "sessions#destroy"
+
   get 'sw.js', to: 'service_worker#service_worker'
+
   "get /   get '/', to:'about#main#index'"
   root to:"main#index"
 end
